5f8372c7ab106abefc71a6317a177b6cc9be16a1
SergioMadrid522 <sergiofabian.madrid@hotmail.com>
2026-04-18 22:01:10 -0600

    fix project title

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,37 @@ import Navbar from "./navbar/Navbar";
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Footer from "./footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ProjectDetail from "./pages/ProjectDetail";
 import ScrollToHashElement from "./utils/ScrollToHashElement";
 
+function AppContent() {
+  const location = useLocation();
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Main />
+          </>
+        }
+      />
+
+      <Route path="/projects/:slug" element={<ProjectDetail />} />
+      {/* <Route path="/freelance/:slug" element={<ProjectDetail />} /> */}
+    </Routes>
+  );
+}
 function App() {
   return (
     <>
       <BrowserRouter>
         <ScrollToHashElement />
         <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Main />
-              </>
-            }
-          />
 
-          <Route path="/projects/:slug" element={<ProjectDetail />} />
-          <Route path="/freelance/:slug" element={<ProjectDetail />} />
-        </Routes>
+        <AppContent />
 
         <Footer />
       </BrowserRouter>
--- src/main/project-section/ProjectSection.tsx
+++ src/main/project-section/ProjectSection.tsx
@@ -1,4 +1,4 @@
-import Freelance from "./Freelance";
+/* import Freelance from "./Freelance"; */
 import ProjectContent from "./ProjectContent";
 
 function ProjectSection() {
@@ -11,12 +11,12 @@ function ProjectSection() {
       <h2>Projects</h2>
 
       <div className="projects-container">
-        <div className="freelance">
+        {/* <div className="freelance">
           <p className="titles">Freelance Projects</p>
           <Freelance />
-        </div>
+        </div> */}
         <div className="selected-projects">
-          <p className="titles">Selected Projects</p>
+          {/* <p className="titles">Selected Projects</p> */}
           <ProjectContent />
         </div>
       </div>
